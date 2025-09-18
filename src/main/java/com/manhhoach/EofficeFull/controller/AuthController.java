f3f216004468b95beed475027c597bf264e1dea9
manhhoach <manhhn.nv5@gmail.com>
2025-09-18 16:56:11 +0700

    .

--- src/main/java/com/manhhoach/EofficeFull/controller/AuthController.java
+++ src/main/java/com/manhhoach/EofficeFull/controller/AuthController.java
@@ -9,13 +9,9 @@ import com.manhhoach.EofficeFull.dto.auth.RegisterReq;
 import com.manhhoach.EofficeFull.dto.auth.LoginRes;
 import com.manhhoach.EofficeFull.dto.user.UserDto;
 import com.manhhoach.EofficeFull.service.AuthService;
-import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.ResponseCookie;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -25,23 +21,10 @@ public class AuthController {
 
     private final AuthService authService;
 
-    @Value("${security.jwt.refresh-token.expiration}")
-    private int refreshTokenExpirationMs;
 
     @PostMapping("/login")
     public ApiResponse<LoginRes> login(@RequestBody LoginReq req, HttpServletResponse response) {
         LoginRes res = authService.login(req);
-
-        ResponseCookie cookie = ResponseCookie.from("myCookieName", "myCookieValue")
-                .path("/") // Set the path for the cookie
-                .sameSite("Lax") // Set SameSite to "Lax", "Strict", or "None"
-                .httpOnly(true) // Optional: Prevents client-side script access
-                .secure(false) // Optional: Ensures the cookie is only sent over HTTPS
-                .maxAge(3600) // Optional: Sets the cookie's max age in seconds
-                .build();
-
-        response.addHeader(HttpHeaders.SET_COOKIE, cookie.toString());
-
         return ApiResponse.success(res);
     }
 
