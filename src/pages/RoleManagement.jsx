8215978459efeb1210c03781e145366e9f0c7d24
manhhoach <manhhn.nv5@gmail.com>
2025-09-28 08:51:37 +0700

    .

--- src/pages/RoleManagement.jsx
+++ src/pages/RoleManagement.jsx
@@ -1,15 +1,19 @@
-import React, { useState } from "react";
-import { Table, Button, Space, Modal, Form } from "antd";
+import React, { useEffect, useState } from "react";
+import { Table, Button, Space, Modal, Form, Input } from "antd";
 import useApi from "../hooks/useApi";
 import CreateRole from "../partials/role/CreateRole";
+import { BiEdit, BiPlus, BiTrash } from "react-icons/bi";
+
+const { Search } = Input;
 
 export default function RoleManagement() {
-    const [pagination, setPagination] = useState({ current: 1, pageSize: 10 });
+    const [pagination, setPagination] = useState({ page: 1, size: 10 });
     const [isModalOpen, setIsModalOpen] = useState(false);
+    const [searchText, setSearchText] = useState("");
 
     const { data, loading, error, refetch } = useApi({
         url: "roles/paged",
-        params: { page: pagination.current, size: pagination.pageSize },
+        params: { page: pagination.current, size: pagination.pageSize, search: searchText },
     });
 
     const columns = [
@@ -28,15 +32,16 @@ export default function RoleManagement() {
             key: "actions",
             render: (_, record) => (
                 <Space>
-                    <Button type="link" onClick={() => console.log("Edit", record)}>
-                        Edit
+                    <Button icon={<BiEdit />} type="link" onClick={() => console.log("Edit", record)}>
+
                     </Button>
                     <Button
                         type="link"
+                        icon={<BiTrash />}
                         danger
                         onClick={() => console.log("Delete", record)}
                     >
-                        Delete
+
                     </Button>
                 </Space>
             ),
@@ -46,9 +51,15 @@ export default function RoleManagement() {
     const handleTableChange = (newPagination) => {
         setPagination(newPagination);
         refetch({
-            params: { page: newPagination.current, size: newPagination.pageSize },
+            params: { page: newPagination.page, size: newPagination.size, search: searchText },
         });
     };
+
+    useEffect(() => {
+        refetch({
+            params: { page: pagination.page, size: pagination.size, search: searchText },
+        });
+    }, [searchText]);
 
     if (error) {
         return <p style={{ color: "red" }}>Error: {error.message}</p>;
@@ -59,13 +70,17 @@ export default function RoleManagement() {
             <h1 style={{ fontSize: 20, fontWeight: "bold", marginBottom: 16 }}>
                 Role Management
             </h1>
-            <Button
-                type="primary"
-                onClick={() => setIsModalOpen(true)}
-                style={{ marginBottom: 16 }}
-            >
-                + Create Role
-            </Button>
+            <div style={{ gap: 16, display: "flex", justifyContent: "space-between" }}>
+                <Search onChange={(e) => { setSearchText(e.target.value) }}></Search>
+                <Button
+                    type="primary"
+                    onClick={() => setIsModalOpen(true)}
+                    icon={<BiPlus />}
+                    style={{ marginBottom: 16 }}
+                >
+                </Button>
+            </div>
+
             <Table
                 rowKey="id"
                 columns={columns}
