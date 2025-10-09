c0995c6e9ff8aa5ad90a883a0166605c14846fd9
manhhoach <manhhn.nv5@gmail.com>
2025-10-09 16:27:16 +0700

    .

--- src/main/java/com/manhhoach/EofficeFull/repository/ProcessStatusRepository.java
+++ src/main/java/com/manhhoach/EofficeFull/repository/ProcessStatusRepository.java
@@ -8,7 +8,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProcessStatusRepository extends JpaRepository<ProcessStatus, Long> {
     @Query("""
             SELECT f from ProcessStatus f WHERE (
--- src/main/java/com/manhhoach/EofficeFull/serviceImpl/ProcessStatusServiceImpl.java
+++ src/main/java/com/manhhoach/EofficeFull/serviceImpl/ProcessStatusServiceImpl.java
@@ -15,7 +15,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
 
+@Service
 public class ProcessStatusServiceImpl implements ProcessStatusService {
 
     @Autowired
