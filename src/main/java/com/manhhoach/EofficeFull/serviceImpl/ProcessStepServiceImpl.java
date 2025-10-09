9e0d8304a1311d8f5b4455f38ccf6e7f409e6a6a
manhhoach <manhhn.nv5@gmail.com>
2025-10-09 22:21:35 +0700

    .

--- src/main/java/com/manhhoach/EofficeFull/serviceImpl/ProcessStepServiceImpl.java
+++ src/main/java/com/manhhoach/EofficeFull/serviceImpl/ProcessStepServiceImpl.java
@@ -1,6 +1,8 @@
 package com.manhhoach.EofficeFull.serviceImpl;
 
 import com.manhhoach.EofficeFull.service.ProcessStepService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class ProcessStepServiceImpl implements ProcessStepService {
 }
